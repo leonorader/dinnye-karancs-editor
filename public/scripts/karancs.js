40db3168dce47a6e95286539f102edfb1e849920
leonorader <dernori@gmail.com>
2020-07-26 21:05:44 +0200

    fix async

--- public/scripts/karancs.js
+++ public/scripts/karancs.js
@@ -1,19 +1,19 @@
 const BASE_URL = 'https://dinnye-karancs.herokuapp.com/api'
 
-function wait(duration) {
+async function wait(duration) {
   return new Promise(resolve => setTimeout(resolve, duration));
 }
 
-function setHotelColour(colour) {
+async function setHotelColour(colour) {
   return axios.put(`${BASE_URL}/hotel/colour`, {colour})
 }
 
-function setHotelMatrix(data) {
+async function setHotelMatrix(data) {
   let matrix = {data: JSON.parse(data)}
   return axios.put(`${BASE_URL}/hotel/matrix`, matrix)
 }
 
-function setRoomColour(floor, room, colour) {
+async function setRoomColour(floor, room, colour) {
   let data = {floor, room, colour}
   return axios.put(`${BASE_URL}/hotel/rooms`, data)
 }
